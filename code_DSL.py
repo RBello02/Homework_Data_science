95a49155966fe00f53400394902d792b4ef6da11
Martina <161827443+MartiChiodo@users.noreply.github.com>
2025-01-16 20:29:11 +0100

    update main con gridsearch

--- code_DSL.py
+++ code_DSL.py
@@ -68,10 +68,9 @@ def main():
     # print(f'The best score is obtained for numfeat = {best_numfeat} and its value is {best_score}')  
 
 
-    # let's see the mask
+    # Determining the MASK
     model = RandomForestRegressor(max_depth= 20, n_estimators= 200)
-    rfe = RFE(model, n_features_to_select= best_numfeat)  
-    rfe.fit(X_train, y_train)
+    rfe = RFE(model, n_features_to_select= best_numfeat, verbose=1, step=5)  
 
     mask_RF = rfe.support_
     features_used_RF = Xdata_dev.columns[mask_RF].to_list()
@@ -79,16 +78,67 @@ def main():
 
     X_train_selected_forest = rfe.transform(X_train)
     X_test_selected_forest = rfe.transform(X_validation)
-
+    
     # Fit the model
     model.fit(X_train_selected_forest, y_train)
 
     # Evaluate the model
     score = root_mean_squared_error(y_validation, model.predict(X_test_selected_forest))
     print(f'numfeat = {best_numfeat} , RMSE = {score} ')
+    
+    
+    # TUNING PARAMS
+    models_and_params = {
+        'RandomForestRegressor': {
+        'model': RandomForestRegressor(),
+        'params': {
+            'n_estimators': [50, 100, 200],
+            'max_depth': [None, 10, 20],
+            'min_samples_split': [2, 5, 10],
+            }
+        },
+    }
+    
+    results = {}
+    
+    model_name = models_and_params.keys()[0]
+    model_details = models_and_params.values[0]
+    
+    print(f"\nEseguendo GridSearchCV per {model_name}...")
+    
+    # Creare la grid search
+    grid_search = GridSearchCV(
+        estimator=model_details['model'],
+        param_grid=model_details['params'],
+        cv=5,  # Numero di fold per la cross-validation
+        scoring='neg_mean_squared_error',  # Usare MSE come metrica di valutazione
+        n_jobs=-1
+    )
+    
+    # Addestrare il modello
+    grid_search.fit(X_train_selected_forest, y_train)
+    
+    # Salvare i risultati
+    results[model_name] = {
+        'best_params': grid_search.best_params_,
+        'best_score': -grid_search.best_score_  # Invertire il segno per MSE
+    }
+    
+    print(f"Migliori parametri per {model_name}: {grid_search.best_params_}")
+    print(f"Miglior MSE (cross-val): {results[model_name]['best_score']:.4f}")
 
-    # Prediction
-    model_pred = RandomForestRegressor()
+    # Valutazione sui dati di test
+    y_pred = grid_search.best_estimator_.predict(X_validation[:, mask_RF])
+    rmse_test = root_mean_squared_error(y_validation, y_pred)
+    r2 = r2_score(y_validation, y_pred)
+
+    print(f"RMSE sui dati di test: {rmse_test:.4f}")
+    print(f"R^2 sui dati di test: {r2:.4f}")
+
+
+
+    # PREDICTION
+    model_pred = grid_search.best_estimator_
     Xdata_dev_masked = Xdata_dev_scaled[:, mask_RF]
     X_data_eval_masked = Xdata_eval_scaled[:, mask_RF]
 
